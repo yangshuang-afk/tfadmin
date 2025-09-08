e464bac70a3bc8c9ba7f0bbde789fe0d47771646
yangshuang-afk <7777719@qq.com>
2025-09-08 11:49:55 +0800

    更新修改密码文件

--- public/components/admin/adminuser/resetPwd.js
+++ public/components/admin/adminuser/resetPwd.js
@@ -1,11 +1,22 @@
 Vue.component('Resetpwd', {
     template: `
-<el-dialog :close-on-click-modal="false" title="重置密码" width="85%" class="icon-dialog" :visible.sync="show" @open="open" :before-close="closeForm" append-to-body>
-        
-        <el-form :size="size" ref="form" :model="form" :rules="rules" :label-width="ismobile()?'90px':'101px'">
-            
-        </el-form>
-        
+		<el-dialog title="重置密码" width="500px" class="icon-dialog" :visible.sync="show" @open="open" :before-close="closeForm" append-to-body>
+			<el-form :size="size" ref="form" :model="form" :rules="rules" label-width="80px">
+				<el-row >
+					<el-col :span="24">
+						<el-form-item label="密码" prop="pwd">
+							<el-input  show-password autoComplete="off" v-model="form.pwd"  clearable placeholder="请输入密码"/>
+						</el-form-item>
+					</el-col>
+				</el-row>
+				<el-row >
+					<el-col :span="24">
+						<el-form-item label="确认密码" prop="repwd">
+							<el-input  show-password autoComplete="off" v-model="form.repwd"  clearable placeholder="请输入确认密码"/>
+						</el-form-item>
+					</el-col>
+				</el-row>
+			</el-form>
 			<div slot="footer" class="dialog-footer">
 				<el-button :size="size" :loading="loading" type="primary" @click="submit" >
 					<span v-if="!loading">确 定</span>
@@ -13,16 +24,13 @@ Vue.component('Resetpwd', {
 				</el-button>
 				<el-button :size="size" @click="closeForm">取 消</el-button>
 			</div>
-
-        
-        
-</el-dialog>
-    `,
+		</el-dialog>
+	`
+    ,
     components:{
-        
     },
     props: {
-                show: {
+        show: {
             type: Boolean,
             default: false
         },
@@ -31,106 +39,74 @@ Vue.component('Resetpwd', {
             default: 'small'
         },
         info: {
-            type: Object
+            type: Object,
         },
     },
-    data() {
+    data(){
+        var validatePass2 = (rule, value, callback) => {
+            if (value === '') {
+                callback(new Error('请再次输入密码'))
+            } else if (value !== this.form.pwd) {
+                callback(new Error('两次输入密码不一致!'))
+            } else {
+                callback()
+            }
+        }
         return {
             form: {
-            user_id: '',
-            name: '',
-            user: '',
-            pwd: '',
-            name: '',
-            role_id: '',
-            status: 1,
-            note: '',
-            create_time: '',
-            session_token: '',
-        },
-role_ids: [],
-
-activeName: '',
-rules: {
-name: [
-{required: true, message: '用户姓名不能为空', trigger: 'blur'}
-],
-user: [
-{required: true, message: '登录账号不能为空', trigger: 'blur'}
-],
-pwd: [
-{required: true, message: '用户密码不能为空', trigger: 'blur'}
-],
-role_id: [
-{required: true, message: '所属角色不能为空', trigger: 'change'}
-],
-status: [
-{required: true, message: '账号状态不能为空', trigger: 'change'}
-]
-        },
-loading: false
-        };
+            },
+            role_ids:[],
+            loading:false,
+            rules: {
+                pwd:[
+                    {required: true, message: '密码不能为空', trigger: 'blur'},
+                ],
+                repwd:[
+                    {required: true, validator: validatePass2, trigger: 'blur'},
+                ],
+            }
+        }
     },
-                            watch:{
-                            
-                 show(val){
-                                if(val){
-                                    axios.post(base_url + '/Adminuser/getFieldList').then(res => {
-                                        if(res.data.status == 200){
-                                           this.role_ids = res.data.data?.role_ids || [] 
-
-                                        }
-                                    })
-                                }
-                            },
-                            
-                            
-                        },
-    methods: {
-        open() {
-    this.form = this.info
-this.form.create_time = parseTime(this.form.create_time)
-},
-submit() {
-    this.$refs['form'].validate(valid => {
-        if (valid) {
-            this.loading = true;
-
-            axios.post(base_url + '/Adminuser/resetPwd', this.form)
-                .then(res => {
-                    if (res.data.status == 200) {
-                        this.$message({message: res.data.msg, type: 'success'});
-                        this.$emit('refesh_list');
-                        this.closeForm();
-                    } else {
-                        this.loading = false;
-                        this.$message.error(res.data.msg);
+    watch:{
+        show(val){
+            if(val){
+                axios.post(base_url + '/Adminuser/getFieldList').then(res => {
+                    if(res.data.status == 200){
+                        this.role_ids = res.data.data.role_ids
                     }
                 })
-                .catch(() => {
-                    this.loading = false;
-                });
-        }
-    });
-},
-normalizer(node) {
-            if (node.children && !node.children.length) {
-                delete node.children
             }
-            return {
-                id: node.val,
-                label: node.key,
-                children: node.children
+        }
+    },
+    methods: {
+        open(){
+            this.form = this.info
+        },
+        submit(){
+            this.$refs['form'].validate(valid => {
+                if(valid) {
+                    this.loading = true
+                    axios.post(base_url + '/Adminuser/resetPwd',this.form).then(res => {
+                        if(res.data.status == 200){
+                            this.$message({message: res.data.msg, type: 'success'})
+                            this.$emit('refesh_list')
+                            this.closeForm()
+                        }else{
+                            this.loading = false
+                            this.$message.error(res.data.msg)
+                        }
+                    }).catch(()=>{
+                        this.loading = false
+                    })
+                }
+            })
+        },
+        closeForm(){
+            this.$emit('update:show', false)
+            this.loading = false
+            if (this.$refs['form']!==undefined) {
+                this.$refs['form'].resetFields()
             }
         },
-closeForm() {
-            this.$emit('update:show', false);
-            this.loading = false;
-            if (this.$refs['form'] !== undefined) {
-                this.$refs['form'].resetFields();
-            }
-            
-			
-        }
     }
-});
+})
