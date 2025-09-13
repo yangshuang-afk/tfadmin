7b1398d4b0f5b1bf466cfa34a19bbffe23b3465c
yangshuang-afk <7777719@qq.com>
2025-09-13 15:30:26 +0800

    更新操作日志记录

--- extend/listen/DoLog.php
+++ extend/listen/DoLog.php
@@ -6,59 +6,89 @@ class DoLog{
     
     //写入操作日志
     public function handle($user){
-        if(in_array(request()->action(),[
-            'add',
-            'update',
-            'delete',
-            'createApplication',
-            'updateApplication',
-            'secrect',
-            'buildApplication',
-            'deleteApplication',
-            'createMenu',
-            'updateMenu',
-            'updateMenuExt',
-            'deleteMenu',
-            'copyMenu',
-            'createField',
-            'batchCreateField',
-            'alterTableField',
-            'updateField',
-            'updateFieldExt',
-            'copyField',
-            'deleteField',
-            'getPostField',
-            'createAction',
-            'quckCreateAction',
-            'updateAction',
-            'updateActionExt',
-            'deleteAction',
-            'create',
-            'createCode',
-            'createByTable',
-            'updateActionRemark',
-            'saveRemarkVersion',
-            'updateFullRemark',
-        ])){
-            $content = request()->except(['s', '_pjax']);
-            if ($content) {
-                foreach ($content as $k => $v) {
-                    if (is_string($v) && strlen($v) > 200 || stripos($k, 'password') !== false) {
-                        unset($content[$k]);
-                    }
+        // if(in_array(request()->action(),[
+        //     'add',
+        //     'update',
+        //     'delete',
+        //     'createApplication',
+        //     'updateApplication',
+        //     'secrect',
+        //     'buildApplication',
+        //     'deleteApplication',
+        //     'createMenu',
+        //     'updateMenu',
+        //     'updateMenuExt',
+        //     'deleteMenu',
+        //     'copyMenu',
+        //     'createField',
+        //     'batchCreateField',
+        //     'alterTableField',
+        //     'updateField',
+        //     'updateFieldExt',
+        //     'copyField',
+        //     'deleteField',
+        //     'getPostField',
+        //     'createAction',
+        //     'quckCreateAction',
+        //     'updateAction',
+        //     'updateActionExt',
+        //     'deleteAction',
+        //     'create',
+        //     'createCode',
+        //     'createByTable',
+        //     'updateActionRemark',
+        //     'saveRemarkVersion',
+        //     'updateFullRemark',
+        // ])){
+        // 	$content = request()->except(['s', '_pjax']);
+        // 	if ($content) {
+        // 		foreach ($content as $k => $v) {
+        // 			if (is_string($v) && strlen($v) > 200 || stripos($k, 'password') !== false) {
+        // 				unset($content[$k]);
+        // 			}
+        // 		}
+        // 	}
+        //
+        // 	$data['application_name'] = app('http')->getName();
+        // 	$data['username'] = $user;
+        // 	$data['url'] = request()->url(true);
+        // 	$data['ip'] = request()->ip();
+        // 	$data['useragent'] = request()->server('HTTP_USER_AGENT');
+        // 	$data['content'] = json_encode($content,JSON_UNESCAPED_UNICODE);
+        // 	$data['create_time'] = time();
+        // 	$data['type'] = 2;
+        //
+        // 	Db::name('log')->insert($data);
+        // }
+        //
+        //
+        // $content = request()->except(['s', '_pjax']);
+        // if ($content) {
+        //     foreach ($content as $k => $v) {
+        //         if (is_string($v) && strlen($v) > 200 || stripos($k, 'password') !== false) {
+        //             unset($content[$k]);
+        //         }
+        //     }
+        // }
+        
+        
+        $content = request()->except(['s', '_pjax']);
+        if ($content) {
+            foreach ($content as $k => $v) {
+                if (is_string($v) && strlen($v) > 200 || stripos($k, 'password') !== false) {
+                    unset($content[$k]);
                 }
             }
-            
-            $data['application_name'] = app('http')->getName();
-            $data['username'] = $user;
-            $data['url'] = request()->url(true);
-            $data['ip'] = request()->ip();
-            $data['useragent'] = request()->server('HTTP_USER_AGENT');
-            $data['content'] = json_encode($content,JSON_UNESCAPED_UNICODE);
-            $data['create_time'] = time();
-            $data['type'] = 2;
-            
-            Db::name('log')->insert($data);
         }
+        $data['application_name'] = app('http')->getName();
+        $data['username'] = $user;
+        $data['url'] = request()->url(true);
+        $data['ip'] = request()->ip();
+        $data['useragent'] = request()->server('HTTP_USER_AGENT');
+        $data['content'] = json_encode($content,JSON_UNESCAPED_UNICODE);
+        $data['create_time'] = time();
+        $data['type'] = 2;
+        
+        Db::name('log')->insert($data);
     }
 }
