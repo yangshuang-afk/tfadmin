3e7fe135133225173e2d3497b3f58067c4265a2a
yangshuang-afk <7777719@qq.com>
2025-11-15 16:15:20 +0800

    优化图片保存路径

--- extend/utils/oss/OssService.php
+++ extend/utils/oss/OssService.php
@@ -11,11 +11,11 @@ class OssService
 	 * @param  string tmpInfo 图片临时文件信息
 	 * @return string oss返回图片完整路径
 	 */
-	public static function OssUpload($file,$rename){
+	public static function OssUpload($file,$rename,$shop_id_folder = ''){
 		if($rename == 2){
-			$filepath = app('http')->getName().'/'.date(config('my.upload_subdir')).'/'.$file->getOriginalName(); //上传路径
+			$filepath = app('http')->getName().'/'.$shop_id_folder.date(config('my.upload_subdir')).'/'.$file->getOriginalName(); //上传路径
 		}else{
-			$filepath = app('http')->getName().'/'.date(config('my.upload_subdir')).'/'.doOrderSn('000').'.'.$file->extension(); //上传路径
+			$filepath = app('http')->getName().'/'.$shop_id_folder.date(config('my.upload_subdir')).'/'.doOrderSn('000').'.'.$file->extension();
 		}
 		
 		switch(config('my.oss_default_type')){
